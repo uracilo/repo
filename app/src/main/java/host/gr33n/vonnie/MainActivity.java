f376c7c127c82e15c73d1c7f5f0fdcaaf842ae10
uracilo <benjamin.casazza@gmail.com>
2017-04-06 10:56:37 -0500

    agregando la app

---
+++ app/src/main/java/host/gr33n/vonnie/MainActivity.java
@@ -0,0 +1,35 @@
+package host.gr33n.vonnie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView myWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+        myWebView = (WebView)findViewById(R.id.activity_main_webview);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        myWebView.loadUrl("https://finalverde.herokuapp.com/");
+        myWebView.setWebViewClient(new WebViewClient());
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (myWebView.canGoBack()) {
+            myWebView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
